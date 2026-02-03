e788b603d47ca4328222066c8e62a166bd4711ab
lycpaul <ycleepaul@gmail.com>
2026-02-03 02:22:02 -0800

    revisiting and fixed my old code

--- TreesGraphs/0104_MaxDepthOfBinaryTree.cpp
+++ TreesGraphs/0104_MaxDepthOfBinaryTree.cpp
@@ -5,26 +5,26 @@
 class Solution
 {
 public:
-    int _depth(TreeNode *root, int height)
+    static int _depth(const TreeNode *root, int height)
     {
         if (root == nullptr) {
             return height - 1;
         }
         return std::max(_depth(root->left, height + 1), _depth(root->right, height + 1));
     }
-    int maxDepth(TreeNode *root) { return _depth(root, 1); }
+    int maxDepth(const TreeNode &root) { return _depth(&root, 1); }
 };
 
 int main()
 {
-    auto *test1 = new TreeNode(3);
-    test1->left = new TreeNode(9);
-    test1->right = new TreeNode(20);
-    test1->right->left = new TreeNode(15);
-    test1->right->right = new TreeNode(7);
+    auto test1 = TreeNode(3);
+    test1.left = new TreeNode(9);
+    test1.right = new TreeNode(20);
+    test1.right->left = new TreeNode(15);
+    test1.right->right = new TreeNode(7);
 
-    auto *test2 = new TreeNode(1);
-    test2->right = new TreeNode(2);
+    auto test2 = TreeNode(1);
+    test2.right = new TreeNode(2);
 
     Solution s;
     std::cout << s.maxDepth(test1) << std::endl;
